cd279dfc1e8d6338b6ad5317ce9aa23c6446ad31
sywong888 <56745380+sywong888@users.noreply.github.com>
2021-02-03 23:06:56 -0800

    choose sound

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,7 +9,33 @@ function changeSlider() {
 
 document.getElementById("honk-btn").addEventListener("click", function(event) {
     event.preventDefault();
-    var audio = new Audio("./assets/media/audio/air-horn.mp3");
+    var sound = selectSound();
+    var filename = "./assets/media/audio/" + sound.substr(6) + ".mp3";
+    var audio = new Audio(filename);
     audio.play();
   });
+
+function selectSound() {
+    const buttons = document.querySelectorAll('input[name="radio-sound"]');
+    let name = "";
+    for (const b of buttons) {
+        if (rb.checked) {
+            name = b.id;
+        }
+    }
+    return name;
+}
+
+// var form = document.querySelector("form");
+// var log = document.querySelector("#log");
+
+// form.addEventListener("submit", function(event) {
+//   var data = new FormData(form);
+//   var output = "";
+//   for (const entry of data) {
+//     output = output + entry[0] + "=" + entry[1] + "\r";
+//   };
+//   log.innerText = output;
+//   event.preventDefault();
+// }, false);
 // TODO
